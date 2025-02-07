31df769f2b3ca90ff82714f8b2f5109ab6c34122
Giovanni Madalozzo <giomadalozzo@MacBook-Pro-de-Giovanni.local>
2025-02-07 12:19:35 -0300

    Initial Commit

---
+++ AgileTV_iOS/AgileTV_iOS/AgileTV_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgileTV_iOSApp.swift
+//  AgileTV_iOS
+//
+//  Created by Giovanni Madalozzo on 07/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AgileTV_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
